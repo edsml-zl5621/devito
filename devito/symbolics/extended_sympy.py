203e1ee571c37a7ca258031960b03fdc3f66cbae
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2023-08-08 18:49:29 +0100

    latest functionpointer

--- devito/symbolics/extended_sympy.py
+++ devito/symbolics/extended_sympy.py
@@ -401,7 +401,7 @@ class FunctionPointer(sympy.Expr, Pickable):
 
     """
     Symbolic representation of C's function pointers to be used as an
-    argument to a function.
+    argument to a function
     """
 
     __rargs__ = ('func_name', 'return_type', 'parameter_type',)
