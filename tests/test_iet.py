8677a933f6cf63ab5bb2f9d554d7896fb6d4e608
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2023-12-13 10:46:24 +0000

    compiler: Add FuncPtrCall

--- tests/test_iet.py
+++ tests/test_iet.py
@@ -10,7 +10,7 @@ from devito import (Eq, Grid, Function, TimeFunction, Operator, Dimension,  # no
 from devito.ir.iet import (Call, Callable, Conditional, DummyExpr, Iteration, List,
                            KernelLaunch, Lambda, ElementalFunction, CGen, FindSymbols,
                            filter_iterations, make_efunc, retrieve_iteration_tree,
-                           Transformer)
+                           Transformer, FuncPtrCall, FindNodes, Definition)
 from devito.ir import SymbolRegistry
 from devito.passes.iet.engine import Graph
 from devito.passes.iet.languages.C import CDataManager
@@ -126,6 +126,26 @@ def test_find_symbols_nested(mode, expected):
     found = FindSymbols(mode).visit(call)
 
     assert [f.name for f in found] == eval(expected)
+
+
+def test_funcptrcall_cgen():
+
+    a = Symbol('a')
+    b = Symbol('b')
+    foo0 = Callable('foo0', Definition(a), 'void', parameters=[b])
+    foo0_arg = FuncPtrCall(foo0.name, foo0.retval, 'int')
+    code0 = CGen().visit(foo0_arg)
+    assert str(code0) == '(void (*)(int))foo0'
+
+    # test nested calls with a FuncPtrCall as an argument
+    call = Call('foo1', [
+        Call('foo2', [foo0_arg])
+    ])
+    code1 = CGen().visit(call)
+    assert str(code1) == 'foo1(foo2((void (*)(int))foo0));'
+
+    callees = FindNodes(Call).visit(call)
+    assert len(callees) == 3
 
 
 def test_list_denesting():
