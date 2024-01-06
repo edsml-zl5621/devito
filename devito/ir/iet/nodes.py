0255c32d2e8f77e7c9a9766d777a96522aed00d1
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-01-06 10:37:33 +0000

    misc: Clean up blank lines

--- devito/ir/iet/nodes.py
+++ devito/ir/iet/nodes.py
@@ -1129,7 +1129,6 @@ class Lambda(Node):
 
 
 class Callback(Call):
-
     """
     Callback as a function pointer.
 
@@ -1151,9 +1150,7 @@ class Callback(Call):
     """
 
     def __init__(self, name, retval, param_types):
-
         super().__init__(name=name)
-
         self.retval = retval
         self.param_types = as_tuple(param_types)
 
