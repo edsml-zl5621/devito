c6be8f66815a8ccddf8ee973b35caf5fb30f086c
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-01-08 17:22:40 +0000

    compiler: Remove manual petsc paths in lower_petsc

--- devito/passes/iet/petsc.py
+++ devito/passes/iet/petsc.py
@@ -11,11 +11,4 @@ def lower_petsc(iet, **kwargs):
     # tmp = FindNodes(Expression).visit(iet)
     # action = [i for i in tmp if i.expr.is_action]
 
-    # add necessary include directories for petsc (TEMP)
-    kwargs['compiler'].add_include_dirs('/home/zl5621/petsc/include')
-    kwargs['compiler'].add_include_dirs('/home/zl5621/petsc/arch-linux-c-debug/include')
-    kwargs['compiler'].add_libraries('petsc')
-    libdir = '/home/zl5621/petsc/arch-linux-c-debug/lib'
-    kwargs['compiler'].add_library_dirs(libdir)
-    kwargs['compiler'].add_ldflags('-Wl,-rpath,%s' % libdir)
     return iet, {'includes': ['petscksp.h', 'petscdmda.h']}
