0275205fe0fa39f4e87a86248ed95743ef113462
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-03-18 16:24:03 +0000

    misc: Clean

---
+++ devito/passes/iet/petsc.py
@@ -0,0 +1,13 @@
+from devito.passes.iet.engine import iet_pass
+
+__all__ = ['lower_petsc']
+
+
+@iet_pass
+def lower_petsc(iet, **kwargs):
+
+    # TODO: This is a placeholder for the actual PETSc lowering.
+    # action_expr = FindNodes(ActionExpr).visit(iet)
+    # rhs_expr = FindNodes(RHSExpr).visit(iet)
+
+    return iet, {}
