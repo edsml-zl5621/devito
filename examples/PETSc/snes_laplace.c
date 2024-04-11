6de0908c92f69d68d78783fec3a23d9bf48fc38e
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-04-11 09:27:50 +0100

    edit snes solve

--- examples/PETSc/snes_laplace.c
+++ examples/PETSc/snes_laplace.c
@@ -66,7 +66,7 @@ int Kernel(struct dataobj *restrict pn_vec, struct dataobj *restrict rhs_vec, co
   // Temporary initialisation and SERIAL only example
   PetscCall(PetscInitialize(NULL,NULL,NULL,NULL));
   PetscCallMPI(MPI_Comm_size(PETSC_COMM_SELF,&(size)));
-  
+
   PetscCall(SNESCreate(PETSC_COMM_SELF, &snes_pn));
   PetscCall(DMDACreate2d(PETSC_COMM_SELF,DM_BOUNDARY_GHOSTED,DM_BOUNDARY_GHOSTED,DMDA_STENCIL_BOX,13,13,PETSC_DECIDE,PETSC_DECIDE,1,2,NULL,NULL,&(da_pn)));
   PetscCall(DMSetUp(da_pn));
@@ -84,7 +84,7 @@ int Kernel(struct dataobj *restrict pn_vec, struct dataobj *restrict rhs_vec, co
   PetscCall(DMCreateLocalVector(da_pn,&(xlocal_pn)));
   PetscCall(DMCreateGlobalVector(da_pn,&(b_pn)));
   PetscCall(VecReplaceArray(xlocal_pn,pn_vec->data));
-  
+
   PetscCall(SNESGetKSP(snes_pn, &ksp_pn));
   PetscCall(KSPSetTolerances(ksp_pn,1.00000000000000e-7F,PETSC_DEFAULT,PETSC_DEFAULT,10000));
   PetscCall(KSPSetType(ksp_pn,KSPGMRES));
@@ -93,8 +93,8 @@ int Kernel(struct dataobj *restrict pn_vec, struct dataobj *restrict rhs_vec, co
   PetscCall(PCJacobiSetType(pc_pn,PC_JACOBI_DIAGONAL));
   PetscCall(KSPSetFromOptions(ksp_pn));
   PetscCall(MatShellSetOperation(J,MATOP_GET_DIAGONAL,(void (*)(void))preconditioner_callback_pn));
-
-  // Setup constant part - rhs
+  
+  // Setup constant part - rhs - in this example it is just zero everywhere
   PetscCall(DMDAVecGetArray(da_pn,b_pn,&b_tmp_pn));
   for (int x = x_m; x <= x_M; x += 1)
   {
@@ -104,9 +104,12 @@ int Kernel(struct dataobj *restrict pn_vec, struct dataobj *restrict rhs_vec, co
     }
   }
   PetscCall(DMDAVecRestoreArray(da_pn,b_pn,&b_tmp_pn));
+  // Set up initial guess using user initialised field - satisfies BCs
+  // This worls because I used VecReplaceArray above.
+  PetscCall(DMLocalToGlobal(da_pn,xlocal_pn,INSERT_VALUES,x_pn));
   PetscCall(SNESSolve(snes_pn,b_pn,x_pn));
   PetscCall(DMGlobalToLocal(da_pn,x_pn,INSERT_VALUES,xlocal_pn));
-  
+
   PetscCall(VecDestroy(&(x_pn)));
   PetscCall(VecDestroy(&(b_pn)));
   PetscCall(MatDestroy(&(J)));
@@ -123,7 +126,7 @@ PetscErrorCode preconditioner_callback_pn(Mat A_matfree_pn, Vec yvec_pn)
   PetscScalar** y_pre_pn;
 
   PetscFunctionBegin;
-  
+
   PetscCall(MatGetDM(A_matfree_pn,&(da_pn)));
   PetscCall(DMGetApplicationContext(da_pn, &ctx));
   PetscCall(DMDAVecGetArray(da_pn,yvec_pn,&y_pre_pn));
@@ -136,7 +139,8 @@ PetscErrorCode preconditioner_callback_pn(Mat A_matfree_pn, Vec yvec_pn)
       y_pre_pn[i0x][i0y] = -2.0*pow(ctx->h_x, -2) - 2.0*pow(ctx->h_y, -2);
     }
   }
-  // Boundary loops - values are just 1 on diagonal (Dirichlet BCs)
+  // Boundary loops - values are 1 on diagonal (Dirichlet BCs)
+  // These loops are the result of using subdomains in Devito.
   for (int x = ctx->x_m; x <= ctx->x_M; x += 1)
   {
     for (int i1y = 1 - ctx->i1y_rtkn + ctx->y_M; i1y <= ctx->y_M; i1y += 1)
@@ -175,7 +179,6 @@ PetscErrorCode FormFunction(SNES snes, Vec xvec_pn, Vec yvec_pn, void *null)
   PetscScalar** y_matvec_pn;
 
   PetscFunctionBegin;
-
   PetscCall(SNESGetDM(snes, &da_pn));
   PetscCall(DMGetApplicationContext(da_pn, &ctx));
 
@@ -184,7 +187,7 @@ PetscErrorCode FormFunction(SNES snes, Vec xvec_pn, Vec yvec_pn, void *null)
   PetscCall(DMGlobalToLocalEnd(da_pn,xvec_pn,INSERT_VALUES,local_xvec_pn));
   PetscCall(DMDAVecGetArrayRead(da_pn,local_xvec_pn,&xvec_tmp_pn));
   PetscCall(DMDAVecGetArray(da_pn,yvec_pn,&y_matvec_pn));
-
+  
   // Interior
   for (int i0x = ctx->i0x_ltkn + ctx->x_m; i0x <= -ctx->i0x_rtkn + ctx->x_M; i0x += 1)
   {
@@ -193,30 +196,31 @@ PetscErrorCode FormFunction(SNES snes, Vec xvec_pn, Vec yvec_pn, void *null)
       y_matvec_pn[i0x][i0y] = -2.0*pow(ctx->h_x, -2)*xvec_tmp_pn[i0x][i0y] + pow(ctx->h_x, -2)*xvec_tmp_pn[i0x - 1][i0y] + pow(ctx->h_x, -2)*xvec_tmp_pn[i0x + 1][i0y] - 2.0*pow(ctx->h_y, -2)*xvec_tmp_pn[i0x][i0y] + pow(ctx->h_y, -2)*xvec_tmp_pn[i0x][i0y - 1] + pow(ctx->h_y, -2)*xvec_tmp_pn[i0x][i0y + 1];
     }
   }
-  // Boundary loops
+  // Boundary loops - F is set to zero.
+  // These loops are the result of using subdomains in Devito.
   for (int x = ctx->x_m; x <= ctx->x_M; x += 1)
   {
     for (int i1y = 1 - ctx->i1y_rtkn + ctx->y_M; i1y <= ctx->y_M; i1y += 1)
     {
-      y_matvec_pn[x][i1y] = xvec_tmp_pn[x][i1y];
+      y_matvec_pn[x][i1y] = 0.;
     }
     for (int i2y = ctx->y_m; i2y <= -1 + ctx->i2y_ltkn + ctx->y_m; i2y += 1)
     {
-      y_matvec_pn[x][i2y] = xvec_tmp_pn[x][i2y];
+      y_matvec_pn[x][i2y] = 0.;
     }
   }
   for (int i3x = ctx->x_m; i3x <= -1 + ctx->i3x_ltkn + ctx->x_m; i3x += 1)
   {
     for (int y = ctx->y_m; y <= ctx->y_M; y += 1)
     {
-      y_matvec_pn[i3x][y] = xvec_tmp_pn[i3x][y];
+      y_matvec_pn[i3x][y] = 0.;
     }
   }
   for (int i4x = 1 - ctx->i4x_rtkn + ctx->x_M; i4x <= ctx->x_M; i4x += 1)
   {
     for (int y = ctx->y_m; y <= ctx->y_M; y += 1)
     {
-      y_matvec_pn[i4x][y] = xvec_tmp_pn[i4x][y];
+      y_matvec_pn[i4x][y] = 0.;
     }
   }
   PetscCall(DMDAVecRestoreArrayRead(da_pn,local_xvec_pn,&xvec_tmp_pn));
@@ -238,7 +242,7 @@ PetscErrorCode MyMatShellMult_pn(Mat A_matfree_pn, Vec xvec_pn, Vec yvec_pn)
 
   PetscCall(MatGetDM(A_matfree_pn,&(da_pn)));
   PetscCall(DMGetApplicationContext(da_pn, &ctx));
-
+  
   PetscCall(DMGetLocalVector(da_pn,&(local_xvec_pn)));
   PetscCall(DMGlobalToLocalBegin(da_pn,xvec_pn,INSERT_VALUES,local_xvec_pn));
   PetscCall(DMGlobalToLocalEnd(da_pn,xvec_pn,INSERT_VALUES,local_xvec_pn));
@@ -253,7 +257,8 @@ PetscErrorCode MyMatShellMult_pn(Mat A_matfree_pn, Vec xvec_pn, Vec yvec_pn)
       y_matvec_pn[i0x][i0y] = -2.0*pow(ctx->h_x, -2)*xvec_tmp_pn[i0x][i0y] + pow(ctx->h_x, -2)*xvec_tmp_pn[i0x - 1][i0y] + pow(ctx->h_x, -2)*xvec_tmp_pn[i0x + 1][i0y] - 2.0*pow(ctx->h_y, -2)*xvec_tmp_pn[i0x][i0y] + pow(ctx->h_y, -2)*xvec_tmp_pn[i0x][i0y - 1] + pow(ctx->h_y, -2)*xvec_tmp_pn[i0x][i0y + 1];
     }
   }
-  // Boundary loops
+  // Boundary loops - equivalent to 1 on diagonal.
+  // These loops are the result of using subdomains in Devito.
   for (int x = ctx->x_m; x <= ctx->x_M; x += 1)
   {
     for (int i1y = 1 - ctx->i1y_rtkn + ctx->y_M; i1y <= ctx->y_M; i1y += 1)
