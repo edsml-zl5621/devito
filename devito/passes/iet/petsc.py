eeafae20300a70ded7b1207ea17197274504d9d3
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-04-26 17:24:59 +0100

    tests: Fix test now that LinSolveExpr is removed

--- devito/passes/iet/petsc.py
+++ devito/passes/iet/petsc.py
@@ -1,6 +1,6 @@
 from devito.passes.iet.engine import iet_pass
-from devito.ir.iet import (FindNodes, MatVecAction, Call, LinearSolverExpression,
-                           Expression, Transformer)
+from devito.ir.iet import (FindNodes, Call, LinearSolverExpression,
+                           Transformer)
 from devito.types import PetscMPIInt
 from devito.symbolics import Byref
 
@@ -17,12 +17,12 @@ def lower_petsc(iet, **kwargs):
     is_petsc = FindNodes(LinearSolverExpression).visit(iet)
 
     if is_petsc:
-        
+
         # Collect all solution fields we're solving for
         targets = [i.expr.rhs.target for i in is_petsc]
 
         # Initialize PETSc i.e generate the one off PETSc calls.
-        init_setup = petsc_setup(targets, **kwargs)
+        init_setup = petsc_setup(targets)
 
         # TODO: Insert code that utilises the metadata attached to each LinSolveExpr
         # that appears in the RHS of each LinearSolverExpression.
@@ -37,25 +37,13 @@ def lower_petsc(iet, **kwargs):
         body = iet.body._rebuild(body=(tuple(init_setup) + iet.body.body))
         iet = iet._rebuild(body=body)
 
-        includes = []
-        # TODO: Obviously it won't be like this.
-        kwargs['compiler'].add_include_dirs('/home/zl5621/petsc/include')
-        path = '/home/zl5621/petsc/arch-linux-c-debug/include'
-        kwargs['compiler'].add_include_dirs(path)
-        kwargs['compiler'].add_libraries('petsc')
-        libdir = '/home/zl5621/petsc/arch-linux-c-debug/lib'
-        kwargs['compiler'].add_library_dirs(libdir)
-        kwargs['compiler'].add_ldflags('-Wl,-rpath,%s' % libdir)
-
-        includes.extend(['petscksp.h', 'petscdmda.h'])
-
-        return iet, {'includes': includes}
-
     return iet, {}
 
 
-def petsc_setup(targets, **kwargs):
+def petsc_setup(targets):
 
+    # Assumption: all targets are generated from the same Grid.
+    communicator = targets[-1].grid.distributor._obj_comm
 
     size = PetscMPIInt(name='size')
 
@@ -63,9 +51,9 @@ def petsc_setup(targets, **kwargs):
     initialize = Call('PetscCall', [Call('PetscInitialize',
                                          arguments=['NULL', 'NULL',
                                                     'NULL', 'NULL'])])
-    
+
     call_mpi = Call('PetscCallMPI', [Call('MPI_Comm_size',
-                                          arguments=['PETSC_COMM_WORLD',
+                                          arguments=[communicator,
                                                      Byref(size)])])
 
     return [initialize, call_mpi]
