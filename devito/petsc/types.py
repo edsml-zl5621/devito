c17f7abfe1ffc43dda2caaa9aa7a861692a566d6
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-05-10 12:36:54 +0100

    compiler: Add core petsc calls to lower_petsc

--- devito/petsc/types.py
+++ devito/petsc/types.py
@@ -368,11 +368,19 @@ def petsc_lift(clusters):
 
 class PETScStruct(CompositeObject):
 
+<<<<<<< HEAD:devito/petsc/types.py
     __rargs__ = ('name', 'usr_ctx')
 
     def __init__(self, name, usr_ctx):
         pfields = [(i._C_name, dtype_to_ctype(i.dtype))
                    for i in usr_ctx if isinstance(i, Symbol)]
+=======
+    __rargs__ = ('name', 'usr_ctx',)
+
+    def __init__(self, name, usr_ctx):
+        pfields = [(i._C_name,
+                    dtype_to_ctype(i.dtype)) for i in usr_ctx if isinstance(i, Symbol)]
+>>>>>>> dedc02610 (compiler: Add core petsc calls to lower_petsc):devito/types/petsc.py
         self._usr_ctx = usr_ctx
         super().__init__(name, 'MatContext', pfields)
 
