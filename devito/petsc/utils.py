34728866e79c1ebba584b4932f0fb700791cf970
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-05-29 15:49:20 +0100

    add utils file in petsc

---
+++ devito/petsc/utils.py
@@ -0,0 +1,7 @@
+from devito.ir.equations import OpMatVec, OpRHS
+from devito.petsc.nodes import MatVecAction, RHSLinearSystem
+
+# Mapping special Eq operations to their corresponding IET Expression subclass types.
+# These operations correspond to subclasses of Eq utilised within PETScSolve.
+iet_mapper = {OpMatVec: MatVecAction,
+              OpRHS: RHSLinearSystem}
