54f5dddd612d941540fc0a1bbeac7ac59eb1d4ac
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-05-29 16:00:41 +0100

    modify init file in iet folder in petsc

--- tests/test_petsc.py
+++ tests/test_petsc.py
@@ -1,5 +1,7 @@
 import numpy as np
 import os
+import pytest
+
 from conftest import skipif
 from devito import Grid, Function, TimeFunction, Eq, Operator, switchconfig
 from devito.ir.iet import (Call, ElementalFunction, Definition, DummyExpr,
@@ -7,17 +9,10 @@ from devito.ir.iet import (Call, ElementalFunction, Definition, DummyExpr,
                            PointerCast, retrieve_iteration_tree)
 from devito.passes.iet.languages.C import CDataManager
 from devito.petsc.types import (DM, Mat, Vec, PetscMPIInt, KSP,
-<<<<<<< HEAD
                                 PC, KSPConvergedReason, PETScArray,
                                 LinearSolveExpr, PETScStruct)
-from devito.petsc.solve import PETScSolve, separate_eqn
+from devito.petsc.solve import PETScSolve, separate_eqn, centre_stencil
 from devito.petsc.iet.nodes import MatVecAction, RHSLinearSystem
-=======
-                                PC, KSPConvergedReason, PETScArray, PETScSolve,
-                                LinearSolveExpr, PETScStruct)
-import os
-from devito.petsc.nodes import MatVecAction, RHSLinearSystem
->>>>>>> 8911a782e (fix circular imports)
 
 
 @skipif('petsc')
