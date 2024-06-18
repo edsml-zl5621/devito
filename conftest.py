5749b2584cbfee8a013c19d8196a96a1a94095fb
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-06-18 14:32:32 +0100

    tests: Utilise petsc_get_dir and petsc_get_arch inside conftest.py

--- conftest.py
+++ conftest.py
@@ -14,6 +14,7 @@ from devito.arch.compiler import (compiler_registry, IntelCompiler, OneapiCompil
 from devito.ir.iet import (FindNodes, FindSymbols, Iteration, ParallelBlock,
                            retrieve_iteration_tree)
 from devito.tools import as_tuple
+from devito.petsc.utils import get_petsc_dir, get_petsc_arch
 
 try:
     from mpi4py import MPI  # noqa
@@ -88,14 +89,18 @@ def skipif(items, whole_module=False):
             skipit = "pyrevolve not installed"
             break
         if i == 'petsc':
-            petsc_dir = os.environ.get('PETSC_DIR', "")
-            petsc_arch = os.environ.get('PETSC_ARCH', "")
-
-            petsc_installed = os.path.join(petsc_dir, petsc_arch,
-                                           'include', 'petscconf.h')
-            if not os.path.isfile(petsc_installed):
-                skipit = "PETSc is not installed"
+            petsc_dir = get_petsc_dir()
+            petsc_arch = get_petsc_arch()
+            if petsc_dir is None or petsc_arch is None:
+                skipit = "PETSC_DIR or PETSC_ARCH are not set"
                 break
+            else:
+                petsc_installed = os.path.join(
+                    petsc_dir, petsc_arch, 'include', 'petscconf.h'
+                )
+                if not os.path.isfile(petsc_installed):
+                    skipit = "PETSc is not installed"
+                    break
 
     if skipit is False:
         return pytest.mark.skipif(False, reason='')
