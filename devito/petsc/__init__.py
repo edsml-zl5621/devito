b700a8d767bffad1b7c213d7a201b4cd75c54f09
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-06-21 16:55:17 +0100

    types: Move PETScsolve to solve file and add separate_eqn functionality

--- devito/petsc/__init__.py
+++ devito/petsc/__init__.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 from devito.petsc.solve import *  # noqa
+=======
+from devito.petsc.solve import *  # noqa
+>>>>>>> 4784cecd4 (types: Move PETScsolve to solve file and add separate_eqn functionality)
