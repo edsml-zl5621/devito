dfde8d0ece99e717dd9fceae2d2f3111b9f4b9c4
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-07-09 14:18:41 +0100

    tests: Add more tests, parameterize, add Derivative dispatch

--- devito/finite_differences/derivative.py
+++ devito/finite_differences/derivative.py
@@ -393,11 +393,6 @@ class Derivative(sympy.Derivative, Differentiable):
     @property
     def _eval_deriv(self):
         return self._eval_fd(self.expr)
-    
-    @property
-    def _eval_deriv_centre(self):
-        centre = [term for term in self._eval_fd(self.expr).args if term.has(self.expr)][0]
-        return centre
 
     def _eval_fd(self, expr, **kwargs):
         """
