55fd465190c3ef9efb236f9e0f7882979539bfe9
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-07-23 12:10:08 +0100

    tests: Move callback test back to test_iet

--- devito/petsc/iet/nodes.py
+++ devito/petsc/iet/nodes.py
@@ -1,4 +1,4 @@
-from devito.ir.iet import Expression, Callable, Call
+from devito.ir.iet import Expression, Callable, Callback
 from devito.ir.equations import OpInjectSolve
 from devito.tools import as_tuple
 
@@ -30,42 +30,7 @@ class PETScCallable(Callable):
         return self._unused_parameters
 
 
-class Callback(Call):
-    """
-    Base class for special callback types.
-
-    Parameters
-    ----------
-    name : str
-        The name of the callback.
-    retval : str
-        The return type of the callback.
-    param_types : str or list of str
-        The return type for each argument of the callback.
-
-    Notes
-    -----
-    The reason Callback is an IET type rather than a SymPy type is
-    due to the fact that, when represented at the SymPy level, the IET
-    engine fails to bind the callback to a specific Call. Consequently,
-    errors occur during the creation of the call graph.
-    """
-    # TODO: Create a common base class for Call and Callback to avoid
-    # having arguments=None here
-    def __init__(self, name, retval, param_types, arguments=None):
-        super().__init__(name=name)
-        self.retval = retval
-        self.param_types = as_tuple(param_types)
-
-    @property
-    def callback_form(self):
-        return
-
-
 class MatVecCallback(Callback):
-    """
-    Callback as a function pointer.
-    """
     @property
     def callback_form(self):
         param_types_str = ', '.join([str(t) for t in self.param_types])
