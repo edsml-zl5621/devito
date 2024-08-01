5eab1161e6d0d39daa9d9b2de4723d6d6a0eea50
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2024-08-01 13:14:40 +0100

    misc: Rebase fix

--- devito/symbolics/printer.py
+++ devito/symbolics/printer.py
@@ -211,9 +211,6 @@ class CodePrinter(C99CodePrinter):
     def _print_Differentiable(self, expr):
         return "(%s)" % self._print(expr._expr)
 
-    def _print_PETScRHS(self, expr):
-        return "%s" % self._print(expr._expr)
-
     _print_EvalDerivative = C99CodePrinter._print_Add
 
     def _print_CallFromPointer(self, expr):
